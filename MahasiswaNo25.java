d269ed61a59b51b51647a98e2a2079380d192a49
kawildan <kawildan01@gmail.com>
2026-02-20 22:44:27 +0700

    jobsheet2

---
+++ MahasiswaNo25.java
@@ -0,0 +1,45 @@
+public class MahasiswaNo25 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    MahasiswaNo25() {
+    }
+    MahasiswaNo25(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+    void tampilkanInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Baik";
+        } else if (ipk >= 2.5) {
+            return "Cukup";
+        } else {
+            return "Kurang";
+        }
+    }
+}
